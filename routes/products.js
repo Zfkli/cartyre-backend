81693766d3d6fe3d69aa670e738d4438e95b8c95
Zfikli <faiqfikli30@gmail.com>
2025-03-02 17:41:39 +0800

    update-products

--- routes/products.js
+++ routes/products.js
@@ -27,6 +27,27 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+//Save edited product
+router.post("/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        const {fld_product_name, fld_price, fld_brand, fld_tyre_size, fld_stock_left, fld_warranty_length, fld_product_image} = req.body;
+
+        const result = await pool.query(
+            `UPDATE tbl_products_a187793 SET fld_product_name = $1, fld_price = $2, fld_brand = $3, fld_tyre_size = $4, fld_stock_left = $5, fld_warranty_length = $6, fld_product_image = 7 WHERE fld_product_id = $8 RETURNING *`,
+            [fld_product_name, fld_price, fld_brand, fld_tyre_size, fld_stock_left, fld_warranty_length, fld_product_image, id]
+        );
+
+        if(result.rows.length === 0) {
+            return res.status(404).json({error: "Product not found"});
+        }
+        res.json({message: "Product udpated successfully", product: result.rows[0]});
+    } catch (error) {
+        console.error("Error udpating product:", error);
+        res.status(500).json({error: "Internal server error"});
+    }
+});
+
 //Add a product
 router.post("/", async(req, res) => {
     const {fld_product_id, fld_product_name, fld_price, fld_brand, fld_tyre_size, fld_stock_left, fld_warranty_length, fld_product_image} = req.body;
